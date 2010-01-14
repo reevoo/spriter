2b9927e78a00b1963988a98603fac71451189646
George Brocklehurst <george.brocklehurst@gmail.com>
2010-01-14 17:42:20 +0000

    Initial commit. Will generate sprite and CSS.

---
+++ lib/spriter.rb
@@ -0,0 +1,60 @@
+require 'mini_magick'
+
+class Spriter
+  class << self
+    attr_accessor :assets_path
+    attr_accessor :sprite_image_path
+    attr_accessor :sprite_image_url
+
+    def transform(css)
+      new(assets_path, sprite_image_path, sprite_image_url).transform(css)
+    end
+
+    def image_dimensions(path)
+      magick_image = MiniMagick::Image.from_file(path)
+      return magick_image[:width], magick_image[:height]
+    end
+  end
+
+  def initialize(assets_path, sprite_image_path, sprite_image_url)
+    @assets_path, @sprite_image_path, @sprite_image_url = assets_path, sprite_image_path, sprite_image_url
+    @images = []
+    @y_offsets = {}
+    @current_offset, @max_width = 0, 0
+  end
+
+  def transform(css)
+    image_matcher = /([^a-z])-spriter-background:\s*([^;]+);/
+    new_css = css.gsub(image_matcher) do |matched|
+      indent = $1
+      image = $2.gsub(/(?:^['"]|['"]$)/, '')
+      add_image(image)
+      "#{indent}background: url(#{@sprite_image_url}) 0 #{-y_offset(image)}px; /* #{image} */"
+    end
+
+    generate_sprite_image
+
+    new_css
+  end
+
+  def add_image(image)
+    return if @images.include? image
+
+    width, height = self.class.image_dimensions(File.join(@assets_path, image))
+
+    @images << image
+    @y_offsets[image] = @current_offset
+    @current_offset += height
+    @max_width = [@max_width, width].max
+  end
+
+  def y_offset(image)
+    @y_offsets[image] or raise ArgumentError, 'Unknown image'
+  end
+
+  def generate_sprite_image
+    return unless @images.any?
+    source_paths = @images.map{ |file| File.join(@assets_path, file) }
+    system(*['convert', source_paths, '-gravity', 'West', '-background', 'transparent', '-append', @sprite_image_path].flatten) or raise "Failed to generate sprite"
+  end
+end
---
+++ test/spriter_test.rb
@@ -0,0 +1,89 @@
+require File.join(File.dirname(__FILE__), *%w[ test_helper ])
+
+class SpriterTest < Test::Unit::TestCase
+
+  def teardown
+    File.delete(Spriter.sprite_image_path) if File.exist? Spriter.sprite_image_path
+  end
+
+  context 'Spriter.image_dimensions' do
+    should 'return the correct dimensions for an image' do
+      width, height = Spriter.image_dimensions(File.join(File.dirname(__FILE__), *%w[ fixtures images green.png ]))
+      assert_equal 50, width
+      assert_equal 20, height
+    end
+  end
+
+  context 'CSS with no -spriter rules' do
+    setup do
+      @css = <<-CSS
+        body {
+          font-family: 'Georgia', 'Times', serif;
+          font-size: small;
+          color: #f00;
+        }
+        p {
+          margin: 0 0 1em 0;
+          background-image: url(/my-spriter-images/badger.gif);
+          border-radius: 10px;
+          -webkit-border-radius: 10px;
+          -moz-border-radius: 10px;
+          -spriter-undefined: 0;
+        }
+      CSS
+
+      @new_css = Spriter.transform(@css)
+    end
+
+    should 'not be changed at all' do
+      assert_equal @css, @new_css
+    end
+
+    should 'not produce a sprite image' do
+      assert_equal false, File.exist?(Spriter.sprite_image_path), 'Expected no sprite image file to be created'
+    end
+  end
+
+  context 'CSS with spriter rules' do
+    setup do
+      @css = <<-CSS
+        .green1 {
+          width: 10px;
+          -spriter-background: 'green.png';
+          height: 20px;
+        }
+        .red {
+          -spriter-background: 'red.png';
+        }
+        .green2 {
+          -spriter-background: 'green.png';
+        }
+      CSS
+
+      @new_css = Spriter.transform(@css)
+    end
+
+    should 'produce the expected CSS' do
+      expected_css = <<-CSS
+        .green1 {
+          width: 10px;
+          background: url(/images/sprites.png) 0 0px; /* green.png */
+          height: 20px;
+        }
+        .red {
+          background: url(/images/sprites.png) 0 -20px; /* red.png */
+        }
+        .green2 {
+          background: url(/images/sprites.png) 0 0px; /* green.png */
+        }
+      CSS
+      assert_equal expected_css, @new_css
+    end
+
+    should 'generate a sprite image with the expected dimensions' do
+      assert File.exist?(Spriter.sprite_image_path), 'Expected a sprite image file to be created'
+      assert_equal [50, 70], Spriter.image_dimensions(Spriter.sprite_image_path)
+    end
+  end
+
+end
