9075b33b9025b184b18ba4001cc1141de9a32e58
George Brocklehurst <george.brocklehurst@gmail.com>
2010-01-18 14:14:36 +0000

    Use the redgreen gem. It's pretty.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,9 @@
 $:.unshift(File.expand_path(File.join(File.dirname(__FILE__), *%w[ .. lib ])))
 
+begin
+  require 'redgreen' unless ENV["TM_PID"]
+rescue MissingSourceFile
+end
+
 require 'shoulda'
 require 'spriter'
