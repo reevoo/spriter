c050453b3c70694be1acc82f7cd4e9d6516b1046
George Brocklehurst <george.brocklehurst@gmail.com>
2010-02-04 12:25:41 +0000

    Allow the stylesheets URL pattern to be customised

--- lib/rack/spriter.rb
+++ lib/rack/spriter.rb
@@ -9,6 +9,7 @@ module Rack
       options = default_options.merge(options)
       @app = app
       @stylesheets_path = options[:stylesheets_path]
+      @stylesheets_url_pattern = options[:stylesheets_url_pattern]
       ::Spriter.assets_path = options[:assets_path]
       ::Spriter.sprite_image_path = options[:sprite_image_path]
       ::Spriter.sprite_image_url = options[:sprite_image_url]
@@ -16,7 +17,8 @@ module Rack
 
     def default_options
       defaults = {
-        :sprite_image_url => '/images/sprites.png'
+        :sprite_image_url => '/images/sprites.png',
+        :stylesheets_url_pattern => %r{stylesheets\/(.+)\.css$}
       }
 
       if defined? Rails
@@ -40,7 +42,7 @@ module Rack
     
     private
     def generate_css(env)
-      if Rack::Request.new(env).path =~ %r{stylesheets\/(.+)\.css$}
+      if Rack::Request.new(env).path =~ @stylesheets_url_pattern
         name = $1
         spriter_path = File.join(@stylesheets_path, "#{name}.spriter")
         generated_css($1) if File.exist? spriter_path
@@ -53,7 +55,7 @@ module Rack
       files = paths.map{ |p| File.new(p, 'r') }
 
       if @generated_css.nil? or files.max{ |a,b| a.mtime <=> b.mtime }.mtime > @generated_at
-        names = paths.map{ |p| p =~ %r{stylesheets/(.+)\.spriter$}; $1 }
+        names = paths.map{ |p| p =~ %r{([^/]+)\.spriter$}; $1 }
         css = ::Spriter.transform(*files)
         css = [css] unless css.is_a? Array
         @generated_at = Time.now
